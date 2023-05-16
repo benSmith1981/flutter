c95e4b2e50af67ef1745d466fa9a0c2e57945590
User_SL8 <benjaminsmith1981@gmail.com>
2023-05-16 11:03:52 +0000

    ADded login and new page

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,86 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  // This function returns true if the username is "admin" and the password is "password",
+  // and false otherwise. In a real app, you would replace this with a call to your
+  // authentication service.
+  Future<bool> checkCredentials(String username, String password) async {
+    return username == 'admin' && password == 'password';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      appBar: AppBar(title: Text('Login')),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (await checkCredentials(
+                  _usernameController.text,
+                  _passwordController.text,
+                )) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MenuPage()),
+                  );
+                } else {
+                  // Show an error message if the login attempt fails.
+                  // Scaffold.of(context).showSshonackBar(
+                  //   SnackBar(content: Text('Invalid username or password')),
+                  // );
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Invalid username or password')),
+                  );
+                }
+              },
+              child: Text('Login'),
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class MenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Menu')),
+      body: Center(child: Text('Welcome to the Menu Page!')),
     );
   }
 }
